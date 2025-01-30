b23602c80a4224510fe807f7efb84eae9a1681b3
W47-KRTS <postuionut47@gmail.com>
2025-01-30 13:02:55 +0200

    class meteor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from os.path import join   
 
-from random import randint  
+from random import randint, uniform  
 
 # ramas la 2:08:00
 
@@ -11,7 +11,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(join('images', 'player.png')).convert_alpha() #stores the surface
         self.rect = self.image.get_frect(midbottom = (window_width/2, window_height / 2))
         self.direction = pygame.Vector2()
-        self.speed = 300
+        self.speed = 400
 
         #cooldown
         self.can_shoot = True
@@ -61,8 +61,17 @@ class Meteor(pygame.sprite.Sprite):
     def __init__(self, surf, pos, *groups):
         super().__init__(*groups)
         self.image = surf
-        self.rect = self.image.get_frect(bottom = pos) # de modif
- 
+        self.rect = self.image.get_frect(center = pos) # de modif
+        self.start_time = pygame.time.get_ticks()
+        self.lifetime = 3000
+        self.direction = pygame.Vector2(uniform(-0.5,0.5), 1)
+        self.speed = randint(350, 500)
+
+    def update(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+        if pygame.time.get_ticks() - self.start_time >= self.lifetime:
+            self.kill()
+
 # general setup
 pygame.init()
 
@@ -87,7 +96,7 @@ player = Player(all_sprites) # create player after the stars
 
 # custom events -> meteor event
 meteor_event = pygame.event.custom_type()
-pygame.time.set_timer(meteor_event, 500) # timer
+pygame.time.set_timer(meteor_event, 400) # timer
 
 while running:
     dt = clock.tick() / 1000
@@ -97,7 +106,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False # variable to close the game
         if event.type == meteor_event:
-            print("create meteor")
+            x, y = randint(0, window_width), randint(-200, -100)
+            Meteor(meteor_surf, (x,y), all_sprites)
      
     # update the game
     all_sprites.update(dt) # call an update method on the sprites in the group
