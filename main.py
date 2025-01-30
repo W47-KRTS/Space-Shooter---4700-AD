7fd6ea5c0fc55aa875090be3219b75f40ad6be66
W47-KRTS <postuionut47@gmail.com>
2025-01-30 11:53:12 +0200

    class Meteor()

--- main.py
+++ main.py
@@ -56,6 +56,14 @@ class Laser(pygame.sprite.Sprite):
         self.rect.centery -= 400 * dt
         if self.rect.bottom <= 0: # the laster left the window game
             self.kill() # destroy laser sprite
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, *groups):
+        super().__init__(*groups)
+        self.image = surf
+        self.rect = self.image.get_frect(bottom = pos) # de modif
+
+ 
 # general setup
 pygame.init()
 
